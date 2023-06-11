438c26336789476b5eb4dcea4ad2388060889efb
Celebrimbor02 <gibson8fla@gmail.com>
2023-06-11 13:24:21 -0300

    Modulo de cadastro de equipamentos criado

--- sistema-fesu/src/main/java/br/org/sistemafesu/controller/EquipamentoController.java
+++ sistema-fesu/src/main/java/br/org/sistemafesu/controller/EquipamentoController.java
@@ -1,8 +1,10 @@
 package br.org.sistemafesu.controller;
 
+import br.org.sistemafesu.entity.Equipamento;
 import br.org.sistemafesu.repository.EquipamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,5 +19,16 @@ public class EquipamentoController {
         return "cadastro-equipamento";
     }
 
+    @RequestMapping(value = "cadastrar-equipamentos", method = RequestMethod.POST)
+    public String Form(Equipamento equipamento){
+        equipamentoRepository.save(equipamento);
+        return "redirect:/cadastrar-equipamentos";
+    }
+
+    @RequestMapping("/equipamentos")
+    public String listarEquipamentos(Model model){
+        model.addAttribute("listaEquipamentos", equipamentoRepository.findAll());
+        return "lista-equipamento";
+    }
 
 }
