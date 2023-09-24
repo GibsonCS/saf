2160412e04a5e81e07f69d98a5ef157e26a428ee
celebrimbor02 <gibson8fla@gmail.com>
2023-09-24 19:23:16 -0300

    add controller usuario mais viewe

--- sistema-fesu/src/main/java/br/org/sistemafesu/repository/UserRepository.java
+++ sistema-fesu/src/main/java/br/org/sistemafesu/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package br.org.sistemafesu.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import br.org.sistemafesu.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
     public User findByUsername(@Param("username")String username);
 }
